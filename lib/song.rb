b13f2920d77c7d6d5e4f1050c39fc2daf215361e
sronga <sr899@georgetown.edu>
2018-07-25 23:41:37 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,8 +10,10 @@ class Song
     self.class.all << self
   end
 
-  def self.create
-    @@all << self.new
+  def self.create(name)
+    song = self.new
+    song.name = name
+    @@all << song
   end
 
 end
