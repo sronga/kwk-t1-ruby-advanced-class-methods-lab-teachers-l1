884f0892645f90a886db021a188ba9145301abd5
sronga <sr899@georgetown.edu>
2018-07-26 00:26:03 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -49,13 +49,15 @@ class Song
   end
 
   def self.new_from_filename(filename)
+    new_song = 
+
     newFile = filename.chomp(".mp3")
 
     data = newFile.split(" - ")
     new_song.artist_name = data[0]
     new_song.name = data[1]
     new_song
-    
+
   end
 
   def self.create_from_filename
