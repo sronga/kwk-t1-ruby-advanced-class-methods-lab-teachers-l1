83441c0481ffb92ba2f31ae7f82516c5ccc3157e
sronga <sr899@georgetown.edu>
2018-07-26 00:17:15 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,7 +38,7 @@ class Song
 
   def self.find_or_create_by_name(name)
     if find_by_name(name) == self.name
-      return find_by_name(name)
+      return self.name
     else
       create_by_name(name)
     end
